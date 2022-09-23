1625e309c776e7961aa60480b4b8ca9e93b8a001
incheol <101618707+icjeon2@users.noreply.github.com>
2022-09-23 15:50:23 +0900

    Update WebActivity.kt

--- WebActivity.kt
+++ WebActivity.kt
@@ -1,6 +1,16 @@
 // Apply this javascript code at the point finished webview loading. (Javascript)
-"document.querySelector(\"meta[name=viewport]\").setAttribute('content', 'width=device-width, initial-scale=0, maximum-scale=2.0, user-scalable=yes');"
-"document.querySelector(\"meta[name=viewport]\").setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes');"
+private val mainWebViewClient by lazy {
+        object : WebViewClient() {
+            override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+            }
+            override fun onPageFinished(view: WebView, url: String) {
+                super.onPageFinished(view, url)
+                "document.querySelector(\"meta[name=viewport]\").setAttribute('content', 'width=device-width, initial-scale=0, maximum-scale=2.0, user-scalable=yes');"
+                "document.querySelector(\"meta[name=viewport]\").setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes');"
+             }
+        }
+}
 
 
 
